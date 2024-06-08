e3abd3786018d0a423f25d78f9fcc1bc2c25b7d5
Parikshit Jadhav <jparikshit2002@gmail.com>
2024-06-08 17:56:56 +0530

    controller and connection.js file added and updated

--- controller/urlcontroller.js
+++ controller/urlcontroller.js
@@ -1,4 +1,30 @@
-const express =require('express')
-const mongoose= require('mongoose')
+const express = require('express');
+const shortid = require('shortid');
+const Url = require('../model/urlSchema');
 
-//we are adding a new branch to our local repo
+exports.handlegenerateNewShortUrl = async (req, res) => {
+    const body = req.body;
+    if (!body.url) return res.status(400).json({ error: "URL is required" });
+    
+    const unique_shortID = shortid.generate(); // corrected the function call
+
+    await Url.create({
+        shortId: unique_shortID,
+        requiredurl: req.body.url,
+        visitHistory: []
+    });
+
+    return res.json({ id: unique_shortID });
+};
+
+exports.handleGetAnalytics = async (req, res) => {
+    const shortId = req.params.shortid; // corrected to match the key in database
+    const result = await Url.findOne({shortId: shortId }); // corrected to match the key in database
+
+    if (!result) {
+        return res.status(404).json({ error: "Short URL not found" });
+    }
+
+    return res.json({ totalClicks: result.visitHistory.length, 
+        analytics: result.visitHistory });
+};
